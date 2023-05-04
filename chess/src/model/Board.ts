529f7d214935b8432cd3761bbaddf2838a789fc2
helenhua9600 <helenhua999@gmail.com>
2023-05-03 22:16:37 -0700

    added hardware.ino for stepper and servo

---
+++ chess/src/model/Board.ts
@@ -0,0 +1,12 @@
+export class Board {
+    private num: number;
+
+    constructor(num: number) {
+        this.num = num;
+    }
+    // test
+
+    public getNum(): number {
+        return this.num;
+    }
+}
